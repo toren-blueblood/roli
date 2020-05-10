40cb3196d910bd7c904369c988a3b83af7e62d17
toren-blueblood <toren.blueblood@protonmail.com>
2020-05-10 03:00:40 -0300

    Create dummy window with SDL.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # roli
 roli para los pibes :D
+
+## Simple DirectMedia Layer
+https://www.libsdl.org/
+```bash
+sudo apt-get install libsdl2-dev
+```
--- server.cpp
+++ server.cpp
@@ -1,6 +1,25 @@
+#include "SDL.h"
 #include <iostream>
 using namespace std;
 
 int main(int argc, char* argv[]) {
     cout << "holi";
+
+    SDL_Window *window;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow("Title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 100, 100, SDL_WINDOW_OPENGL);
+    if (window == NULL) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Delay(5000);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    return 0;
 }
