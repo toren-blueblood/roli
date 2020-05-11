60e4fc58ba36826a52537ebc79c767c8c321e2a1
toren-blueblood <toren.blueblood@protonmail.com>
2020-05-11 15:35:09 -0300

    Render dummy image.

--- server.cpp
+++ server.cpp
@@ -1,25 +1,56 @@
 #include "SDL.h"
-#include <iostream>
-using namespace std;
+
 
 int main(int argc, char* argv[]) {
-    cout << "holi";
-
-    SDL_Window *window;
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return 1;
     }
 
-    window = SDL_CreateWindow("Title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 100, 100, SDL_WINDOW_OPENGL);
-    if (window == NULL) {
-        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+    SDL_Window *window = SDL_CreateWindow("Title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
+    if (window == nullptr) {
+        SDL_Log("SDL_CreateWindow Error: %s", SDL_GetError());
+        SDL_Quit();
         return 1;
     }
 
-    SDL_Delay(5000);
-    SDL_DestroyWindow(window);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr){
+        SDL_Log("SDL_CreateRenderer Error: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
 
+    SDL_Surface *surface = SDL_LoadBMP("../resources/holi.bmp");
+    if (surface == nullptr){
+        SDL_Log("SDL_LoadBMP Error: %s", SDL_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (texture == nullptr){
+        SDL_Log("SDL_CreateTextureFromSurface Error: %s", SDL_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Rect rect;
+    SDL_GetClipRect(surface, &rect);
+    SDL_FreeSurface(surface);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, nullptr, &rect);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(5000);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
